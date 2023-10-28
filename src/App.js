092f8ff3e7d19aca0024acbd838e604bde4c06ce
Sidharth Naik <121108780+sidharthnaik003@users.noreply.github.com>
2023-10-28 21:41:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,4 @@
-// import React from 'react'
-// import Header from './Component/Header'
-// import Body from './Component/Body'
-// import Footer from './Component/Footer'
-// //import Bootstrap from './Component/Bootstrap'
-// //import Navbar from './Component/Navbar'
-// const App = () => {
-//   return (
-//     <>
 
-//       <Header/>
-//       <Body />
-//       <Footer />
-
-//     </>
-//   );
-
-// };
-
-// export default App ;
 
 import React from "react";
 import Home from "./Pages/Home";
